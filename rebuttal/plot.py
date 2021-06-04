2b4d062abfa5b215ddda5c88991789bd7b0ebe99
KamalakerDadi <dkamalakarreddy@gmail.com>
2021-06-04 16:10:32 +0200

    update

--- rebuttal/plot.py
+++ rebuttal/plot.py
@@ -1,52 +1,39 @@
+"""Plot learning curves on the save results
+
+   Results are R-squared scores meaned/std across 100 CV splits
+"""
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df = pd.read_csv('learning_curves.csv')
+targets = df['target'].unique()
+learning_types = df['learning_type'].unique()
 
-def compute_mean_std_r2_scores(scores):
-    """
-    """
-    scores_mean = scores.mean(axis=1)
-    scores_std = scores.std(axis=1)
-    
-    return scores_mean, scores_std
-
-
+# plotting
 plt.style.use('seaborn')
-
-fig, axes = plt.subplots(ncols=3, sharey=True)
-
-aliases = {'age': 'Age',
-           'fluid_intelligence': 'Fluid intelligence',
-           'neuroticism': 'Neuroticism'}
-
-train_sizes = {'age': [100, 500, 1000, 1500, 2000, 2500, 3000, 3500, 3700],
-               'fluid_intelligence': [100, 500, 1000, 1500, 2000, 2500, 3000, 3500, 3700],
-               'neuroticism': [100, 500, 1000, 1500, 2000, 2500, 3000, 3195]}
-
-targets = ['age', 'fluid_intelligence', 'neuroticism']
 
 colors = {'train': 'r',
           'validation': 'g'}
 
-for curve_type in ['train', 'validation']:
-    color = colors[curve_type]
+fig, axes = plt.subplots(figsize=(10, 4), ncols=4, sharey=True)
+
+for learning_type in learning_types:
+    this_type_data = df[df['learning_type'] == learning_type]
     for i, (ax, target) in enumerate(zip(axes, targets)):
-        this_sizes = train_sizes[target]
-        scores = pd.read_csv('{}_scores_predicted_{}.csv'.format(curve_type,
-                                                                 target))
-        scores = scores.drop('Unnamed: 0', axis=1)
-        scores_mean, scores_std = compute_mean_std_r2_scores(scores)
-        ax.plot(train_sizes[target], scores_mean, 'o-',
-                color=color, label = curve_type)
-        ax.fill_between(this_sizes, scores_mean - scores_std,
-                        scores_mean + scores_std, alpha=0.2,
-                        color=color)
+        this_target = this_type_data[this_type_data['target'] == target]
+        ax.plot(this_target['train_sizes'], this_target['mean'],
+                'o-', color=colors[learning_type],
+                label = learning_type)
+        ax.fill_between(this_target['train_sizes'],
+                this_target['mean'] - this_target['std'],
+                this_target['mean'] + this_target['std'],
+                alpha=0.2, color=colors[learning_type])
         if i == 0:
             ax.set_ylabel('$R^{2}$', fontsize = 18)
-        elif i == 1:
+        elif i == 2:
             ax.set_xlabel('Training sizes', fontsize = 18)
-        ax.set_title(aliases[target], fontsize=18)
+        ax.set_title(target, fontsize=18)
 plt.legend()
 plt.tight_layout()
-plt.savefig('learning_curves.png', bbox_inches='tight')
-plt.savefig('learning_curves.pdf', bbox_inches='tight')
+plt.show()
+
