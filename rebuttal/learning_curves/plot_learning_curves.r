752f5f6cff133e514837ff1e71c9537037ea8fb0
Denis A. Engemann <denis-alexander.engemann@inria.fr>
2021-06-30 00:38:20 +0200

    update learning curves

--- rebuttal/learning_curves/plot_learning_curves.r
+++ rebuttal/learning_curves/plot_learning_curves.r
@@ -6,8 +6,8 @@ source('../../config.r')
 df <- fread('learning_curves.csv')
 df[,learning_type := factor(learning_type, levels = c("train", "validation"))] 
 df[target=="Age at assessment", target := "Age"]
-df[learning_type=="train", learning_type := "trainig"]
-df[learning_type=="validation", learning_type := "testing"]
+df[learning_type=="train", learning_type := "Validation"]
+df[learning_type=="validation", learning_type := "Generalization"]
 
 fig <- ggplot(
   data = df,
@@ -16,7 +16,8 @@ fig <- ggplot(
   facet_wrap(.~target) +
   geom_line(size = 1.3) +
   geom_ribbon(mapping = aes(ymin = mean -std, ymax = mean + std),
-              alpha = 0.2, size = 0.2) +
+            alpha = 0.2, size = 0.2) +
+  scale_color_manual(values = with(color_cats, c(vermillon, `sky blue`))
   my_theme +
   labs(x = "# training samples", y = expression(R^2 ~ "score")) +
   theme(legend.title = element_blank(), legend.position="bottom")
